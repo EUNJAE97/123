6bba4f97611965d73e9c4fb29b447597cde3021b
EUNJAE97 <154950170+EUNJAE97@users.noreply.github.com>
2024-01-14 22:41:36 +0900

    Add files via upload

---
+++ 슬라임/slime-heroes-project/src/com/slimeheroes/hero/Hero.java
@@ -0,0 +1,16 @@
+package com.slimeheroes.hero;
+
+import com.slimeheroes.slimes.Slimes;
+
+public abstract class Hero {
+    int heroHp = 100;
+    int skillDamage;
+
+    public abstract void classSkill(Slimes slimes);
+
+    public abstract void defend();
+
+    public abstract int getHeroHp();
+
+    public abstract void setHeroHp(int heroHp);
+}
---
+++ 슬라임/slime-heroes-project/src/com/slimeheroes/slimes/Slimes.java
@@ -0,0 +1,23 @@
+package com.slimeheroes.slimes;
+
+import com.slimeheroes.hero.Hero;
+
+public abstract class Slimes {
+    
+
+
+
+    public abstract void attack(Hero hero);
+
+    public abstract void halfDamageAttack(Hero hero);
+
+
+    public abstract int getSlimeHp();
+
+
+
+    public abstract void setSlimeHp(int slimeHp);
+
+    public void finalattack(Hero hero) {
+    }
+}
